31f518b300bbbc2f97dcd8d1255ac3fe192dc468
megan13oneill <psmoneil@liverpool.ac.uk>
2025-07-31 15:21:29 +0100

    taken out context manager

--- connect.py
+++ connect.py
@@ -7,7 +7,6 @@ from error_logger import log_error_to_file
 # OPC UA server endpoint for the Mettler Toldeo IR Flow cell. 
 SERVER_URL = "opc.tcp://localhost:62552/iCOpcUaServer"
 
-@contextmanager
 def try_connect(server_url=SERVER_URL, max_retries=3, delay=2, error_log_path=None):
 
     """ Will attempt to connect to the OPC UA server. Will either return connected if successful 
@@ -15,34 +14,28 @@ def try_connect(server_url=SERVER_URL, max_retries=3, delay=2, error_log_path=No
     """
 
     client = Client(server_url)
-    connected = False
-
-    try:
-        for attempt in range(1, max_retries + 1):
-            try:
-                client.connect()
-                connected = True
-                print(f"Connected to OPC UA server at {server_url} (Attempt {attempt})")
-                yield client
-                break
-            except Exception as e:
-                error_message = f"Attempt {attempt} failed to connect to {server_url}"
-                print(f"{error_message}: {e}")
-                if error_log_path:
-                    log_error_to_file(error_log_path, error_message, e)
-
-                if attempt < max_retries:
-                    time.sleep(delay)   # wait before next attempt
-                else:
-                    print("All connection attempts failed.")
-                    yield None
-    finally:
-        if connected:
-            try:
-                client.disconnect()
-                print("Disconnected from OPC UA server.")
-            except Exception as e:
-                if error_log_path:
-                    log_error_to_file(error_log_path, "Error during OPC UA disconnection", e)
+    for attempt in range(1, max_retries + 1):
+        try:
+            client.connect()
+            print(f"Connected to OPC UA server at {server_url} (Attempt {attempt})")
+            return client
+        except Exception as e:
+            error_message = f"Attempt {attempt} failed to connect to {server_url}"
+            print(f"{error_message}: {e}")
+            if error_log_path:
+                log_error_to_file(error_log_path, error_message, e)
+            if attempt < max_retries:
+                time.sleep(delay)   # wait before next attempt
+            else:
+                print("All connection attempts failed.")
+                return None
+# finally:
+#     if connected:
+#         try:
+#             client.disconnect()
+#             print("Disconnected from OPC UA server.")
+#         except Exception as e:
+#             if error_log_path:
+#                 log_error_to_file(error_log_path, "Error during OPC UA disconnection", e)
 
 
